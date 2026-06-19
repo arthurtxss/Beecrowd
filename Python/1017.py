bb5f25c565d2b31a9fcae53716b34ce22b8f6b85
arthur teixeira <arthurtxss@gmail.com>
2026-06-18 21:24:42 -0300

    1017 - Gasto de Combustível

---
+++ Python/1017.py
@@ -0,0 +1,9 @@
+carro = 12
+tempo = input()
+tempo = int(tempo)
+velocidade= input()
+velocidade= int (velocidade)
+velocidade = int(velocidade)
+
+litros = (((tempo* velocidade))/carro)
+print(f"{litros:.3f}")
